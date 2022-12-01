c3b74c077345e9663889366206070f9bfcc0fedd
muniz079 <115712016+muniz079@users.noreply.github.com>
2022-12-01 12:13:46 -0300

    Add files via upload

---
+++ dia primeiro de dezembro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvocaciaPrincipal.modelo
+{
+    public class Advogado
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+
+        public bool verificarAdvogadoEMaiorIdade()
+        {
+            if (this.Idade >= 18)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<Advogado> getAllAdvogados()
+        {
+            return null;
+        }
+        public bool eExperiente()
+        {
+            return false;
+        }
+}
